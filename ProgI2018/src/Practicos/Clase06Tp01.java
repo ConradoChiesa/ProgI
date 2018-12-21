d206f395725a82f4cb101cfcc3d71a022531977b
Conrado Chiesa <conradochiesa@gmail.com>
2018-12-20 21:48:23 -0300

    Practicos 1 y 2 clase 06, falta el 3

---
+++ ProgI2018/src/Practicos/Clase06Tp01.java
@@ -0,0 +1,37 @@
+/*Escribir un método que retorne el mayor de dos números. Usar
+ese método para calcular el máximo de una serie de números
+ingresados por el usuario (al menos 10 números).*/
+
+package Practicos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Clase06Tp01 {
+
+	public static void main(String[] args) {
+		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			System.out.println("ingrese un número");
+			int a = new Integer(entrada.readLine());
+			System.out.println("ingrese un número");
+			int b = new Integer(entrada.readLine());
+			int c = mayor(a,b);
+			System.out.println("El número mayor es: " + c);
+		}
+			catch (Exception exc ){
+				System.out.println( exc );
+			}
+	}
+	public static int mayor (int a, int b) {
+		int aux;
+		if (a>b) {
+			aux = a;
+		} else {
+			aux = b;
+		}
+		return aux;
+		}
+
+}
---
+++ ProgI2018/src/Practicos/Clase06Tp02.java
@@ -0,0 +1,58 @@
+/*Escribir un programa que simule el lanzamiento de un dado
+(1000 veces) y muestre por pantalla cuantas veces salió el valor
+del dado correspondiente al número entero N ingresado por el
+usuario. Considerar el valor N ingresado se corresponda a un
+valor posible para un dado. Usar la sentencia Math.random()
+que devuelve un valor aleatorio real entre 0 y 1.
+ Ejemplo: para asignar un posible valor a la variable dado entero:
+dado = (int) (6*Math.random() + 1)*/
+
+package Practicos;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Clase06Tp02 {
+
+	final static int MAX = 1000;
+	final static int min = 0;
+	public static void main(String[] args) {
+		
+		int selection = pedir_numero();
+		int probabilidad = tirar_dado(selection);
+		System.out.println("La selección del número " + selection + " salio " + probabilidad + " veces.");
+		
+	}
+	
+	
+	private static int tirar_dado(int selection) {
+		int counting = 0;
+		for (int i=min;i<MAX;i++) {
+			int dado = 0;
+			dado = (int) (6*Math.random() + 1);
+			if (dado == selection) {
+				counting++;
+			}
+		}
+
+		return counting;
+	}
+
+	public static int pedir_numero() {
+		int selection = 0;
+		BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			do {
+				System.out.println("ingrese un número entre 1 y 6");
+				selection = new Integer(entrada.readLine());
+				
+			} while (!((selection>0)&&(selection<7)));
+			System.out.println(selection);
+		}
+		catch (Exception exc ){
+			System.out.println( exc );
+		}
+		return selection;
+	}
+
+}
